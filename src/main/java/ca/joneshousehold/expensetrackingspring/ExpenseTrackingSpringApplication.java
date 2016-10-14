ac143fa53fe55e063d357df443a4ecb27e150af2
Andrew Jones <contactandrewjones@gmail.com>
2016-10-14 15:19:05 -0400

    initial commit

---
+++ src/main/java/ca/joneshousehold/expensetrackingspring/ExpenseTrackingSpringApplication.java
@@ -0,0 +1,12 @@
+package ca.joneshousehold.expensetrackingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackingSpringApplication.class, args);
+	}
+}
